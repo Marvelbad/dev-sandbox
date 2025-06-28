241a52b24d4e6465c0d7eb3f3d58e88a5cd4fd3a
Marvelbad <badra.aqua@gmail.com>
2025-06-28 20:52:53 +0300

    Practice

--- src/main/java/badri/sandbox/core/practice/Solution.java
+++ src/main/java/badri/sandbox/core/practice/Solution.java
@@ -1,29 +1,34 @@
 package badri.sandbox.core.practice;
 
-import lombok.var;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter a text: ");
-        String s = console.readLine();
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(s))) {
-            String line;
-            HashMap<String, Integer> map = new HashMap<>();
-            while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(" ");
+        Map<Integer, Map<String, Integer>> outerMap = new HashMap<>();
+        int lineNumber = 1;
+        while (true) {
+            String str = console.readLine();
+            if (str.isBlank() || str.equals("end")) break;
 
-                for (String word : parts) {
-                    int count = map.getOrDefault(word, 0);
-                    map.put(word, count + 1);
+            Map<String, Integer> innerMap = outerMap.getOrDefault(lineNumber, new HashMap<>());
+            String[] parts = str.split(" ");
+            for (String part : parts) {
+                if (part.matches("[a-zA-Zа-яА-Я0-9]+")) {
+                    innerMap.put(part, innerMap.getOrDefault(part, 0) + 1);
                 }
             }
+            outerMap.put(lineNumber, innerMap);
+            lineNumber++;
+        }
+        for (Map.Entry<Integer, Map<String, Integer>> entry : outerMap.entrySet()) {
+            System.out.println(entry.getKey() + entry.getValue().toString());
         }
     }
 }
