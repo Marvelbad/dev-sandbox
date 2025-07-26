15654e10742d7cbc7429498b023a2d4b60d78125
Marvelbad <badra.aqua@gmail.com>
2025-07-26 22:28:35 +0300

    Practice

--- src/main/java/badri/sandbox/core/practice/Solution.java
+++ src/main/java/badri/sandbox/core/practice/Solution.java
@@ -1,63 +1,19 @@
 package badri.sandbox.core.practice;
 
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Solution {
-    public static void main(String[] args) throws Exception {
-        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-        String str = console.readLine();
-        String[] strings = null;
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Badri", "Alex", "Aleksander", "Sergey", "Tom");
+        List<String> resultNames = names.stream()
+                .filter(s-> s.length() > 3)
+                .map(String::toUpperCase)
+                .toList();
+        resultNames.forEach(System.out::println);
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(str))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                strings = line.split(" ");
-            }
-        }
-
-        StringBuilder result = getLine(strings);
-        System.out.println(result);
-    }
-
-    public static StringBuilder getLine(String... words) {
-        if (words == null || words.length == 0) return new StringBuilder();
-
-        List<StringBuilder> builderList = new ArrayList<>();
-
-        for (String word : words) {
-            List<String> list = new ArrayList<>(Arrays.asList(words));
-            list.remove(word);
-
-            StringBuilder builder = new StringBuilder(word);
-            int count = 0;
-            while (!list.isEmpty()) {
-                String firstStartChar = builder.substring(0, 1);
-                String firstEndChar = builder.substring(builder.length() - 1);
-                String str = list.get(0);
-                String secondStartChar = str.substring(0, 1);
-                String secondEndChar = str.substring(str.length() - 1);
-
-                if (firstEndChar.equalsIgnoreCase(secondEndChar)) {
-                    builder.append(" ").append(str);
-                    list.remove(0);
-                } else if (firstStartChar.equalsIgnoreCase(secondStartChar)) {
-                    builder.insert(0, str + " ");
-                    list.remove(0);
-                } else {
-                    list.remove(str);
-                    list.add(str);
-                    count++;
-                    if (count > Math.pow(words.length, 2)) break;
-                }
-            }
-            builderList.add(builder);
-        }
-        builderList.forEach(System.out::println);
-        return builderList.stream()
-                .max(Comparator.comparingInt(StringBuilder::length)).get();
     }
 }
