ceecec3c9c128a9516ff4e9484ecef29ce674dd4
Marvelbad <badra.aqua@gmail.com>
2025-07-30 17:14:20 +0300

    Practice

--- src/main/java/badri/sandbox/core/practice/Solution.java
+++ src/main/java/badri/sandbox/core/practice/Solution.java
@@ -1,19 +1,51 @@
 package badri.sandbox.core.practice;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+public class Solution implements Action {
+    public static int countActionObjects;
 
-public class Solution {
+    private int param;
+
+    private Action solutionAction = new Action() {
+        @Override
+        public void someAction() {
+            if (param > 0) {
+                FirstClass firstClass = new FirstClass() {
+                    @Override
+                    public Action getDependantAction() {
+                        param--;
+                        if (param > 0) {
+                            return solutionAction; // recursion
+                        } else {
+                            return new SecondClass();
+                        }
+                    }
+                };
+                firstClass.someAction();
+                firstClass.getDependantAction().someAction();
+            } else {
+                new SecondClass().someAction();
+            }
+        }
+    };
+
+    public Solution(int param) {
+        this.param = param;
+    }
+
+
+
     public static void main(String[] args) {
-        List<String> names = Arrays.asList("Badri", "Alex", "Aleksander", "Sergey", "Tom");
-        List<String> resultNames = names.stream()
-                .filter(s-> s.length() > 3)
-                .map(String::toUpperCase)
-                .toList();
-        resultNames.forEach(System.out::println);
+        Solution solution = new Solution(5);
+        solution.someAction();
+        System.out.println("Count of created Action objects is " + countActionObjects);
+
+        solution = new Solution(-1);
+        solution.someAction();
+        System.out.println("Count of created Action objects is " + countActionObjects);
+    }
+
+    @Override
+    public void someAction() {
 
     }
 }
