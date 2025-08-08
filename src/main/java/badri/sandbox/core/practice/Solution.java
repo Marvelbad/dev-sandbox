dcf51affb1bc4f96d74e0d624e86c0a9e7833f3a
Marvelbad <badra.aqua@gmail.com>
2025-08-08 20:34:32 +0300

    Practice

--- src/main/java/badri/sandbox/core/practice/Solution.java
+++ src/main/java/badri/sandbox/core/practice/Solution.java
@@ -10,8 +10,24 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class Solution {
+
     public static void main(String[] args) throws Exception {
-        Runnable task = () -> System.out.println("Hello");
-        new Thread(task).start();
+        class EmulatorThreadFactoryTask implements Runnable {
+            @Override
+            public void run() {
+
+            }
+        }
+    }
+
+    ThreadGroup group = new ThreadGroup("firstGroup");
+    Thread thread = new Thread(group, new EmulatorThreadFactoryTask());
+
+    ThreadGroup group2 = new ThreadGroup("secondGroup");
+    Thread thread2 = new Thread(group2, new EmulatorThreadFactoryTask());
+
+
+    private static void emulateThreadFactory() {
+        
     }
 }
