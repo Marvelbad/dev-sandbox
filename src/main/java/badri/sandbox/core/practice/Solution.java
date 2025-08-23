76b61cbeb187904d2769c8e463abb82354ebda2f
Marvelbad <badra.aqua@gmail.com>
2025-08-23 20:58:13 +0300

    Practice

--- src/main/java/badri/sandbox/core/practice/Solution.java
+++ src/main/java/badri/sandbox/core/practice/Solution.java
@@ -4,30 +4,45 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Solution {
 
     public static void main(String[] args) throws Exception {
-        class EmulatorThreadFactoryTask implements Runnable {
+        class EmulatorThreadFactory implements Runnable {
             @Override
             public void run() {
-
+                emulateThreadFactory();
             }
         }
     }
 
-    ThreadGroup group = new ThreadGroup("firstGroup");
-    Thread thread = new Thread(group, new EmulatorThreadFactoryTask());
-
-    ThreadGroup group2 = new ThreadGroup("secondGroup");
-    Thread thread2 = new Thread(group2, new EmulatorThreadFactoryTask());
-
-
     private static void emulateThreadFactory() {
-        
+        AmigoThreadFactory factory = new AmigoThreadFactory();
+        Runnable r = () -> System.out.println(Thread.currentThread().getName());
+        factory.newThread(r).start();
+        factory.newThread(r).start();
+        factory.newThread(r).start();
+    }
+
+    public static class AmigoThreadFactory implements ThreadFactory {
+        private static AtomicInteger factoryNumber = new AtomicInteger(1);
+        private AtomicInteger threadNumber = new AtomicInteger(1);
+        private ThreadGroup threadGroup;
+
+        private String format;
+
+        public AmigoThreadFactory() {
+            threadGroup = Thread.currentThread().getThreadGroup();
+            format = String.format("%s-pool-%s-thread-", threadGroup.getName(), factoryNumber.getAndIncrement());
+        }
+
+        public Thread newThread(Runnable runnable) {
+            Thread thread = new Thread(runnable, format + threadNumber.getAndIncrement());
+            thread.setDaemon(false);
+            thread.setPriority(Thread.NORM_PRIORITY);
+            return thread;
+        }
     }
 }
