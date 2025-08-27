4b5670713af68f6b254798a33bb56f8da189dbce
Marvelbad <badra.aqua@gmail.com>
2025-08-27 13:48:33 +0300

    Practice

--- src/main/java/badri/sandbox/core/practice/Solution.java
+++ src/main/java/badri/sandbox/core/practice/Solution.java
@@ -1,48 +1,12 @@
 package badri.sandbox.core.practice;
 
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicInteger;
+import badri.sandbox.core.practice.ui.Player;
+import badri.sandbox.core.practice.ui.UI;
 
 public class Solution {
-
-    public static void main(String[] args) throws Exception {
-        class EmulatorThreadFactory implements Runnable {
-            @Override
-            public void run() {
-                emulateThreadFactory();
-            }
-        }
-    }
-
-    private static void emulateThreadFactory() {
-        AmigoThreadFactory factory = new AmigoThreadFactory();
-        Runnable r = () -> System.out.println(Thread.currentThread().getName());
-        factory.newThread(r).start();
-        factory.newThread(r).start();
-        factory.newThread(r).start();
-    }
-
-    public static class AmigoThreadFactory implements ThreadFactory {
-        private static AtomicInteger factoryNumber = new AtomicInteger(1);
-        private AtomicInteger threadNumber = new AtomicInteger(1);
-        private ThreadGroup threadGroup;
-
-        private String format;
-
-        public AmigoThreadFactory() {
-            threadGroup = Thread.currentThread().getThreadGroup();
-            format = String.format("%s-pool-%s-thread-", threadGroup.getName(), factoryNumber.getAndIncrement());
-        }
-
-        public Thread newThread(Runnable runnable) {
-            Thread thread = new Thread(runnable, format + threadNumber.getAndIncrement());
-            thread.setDaemon(false);
-            thread.setPriority(Thread.NORM_PRIORITY);
-            return thread;
-        }
+    public static void main(String[] args) {
+        Player player = new Player();
+        UI ui = new UI(player);
+        ui.init();
     }
 }
