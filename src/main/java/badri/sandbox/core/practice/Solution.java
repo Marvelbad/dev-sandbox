59f769b82f2d9c1730487ff23a59719e99f44c6c
Marvelbad <badra.aqua@gmail.com>
2025-10-19 18:23:18 +0300

    Practice

--- src/main/java/badri/sandbox/core/practice/Solution.java
+++ src/main/java/badri/sandbox/core/practice/Solution.java
@@ -1,29 +1,15 @@
 package badri.sandbox.core.practice;
 
-
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.stream.Collectors;
+import java.awt.print.Book;
 
 public class Solution {
     public static void main(String[] args) {
-        Map<String, Integer> map = Map.of("a", 1,
-                "b", 2,
-                "c", 3,
-                "d", 4,
-                "e", 5
-        );
-
-        Map<String, Integer> sortedMap = map.entrySet()
-                .stream()
-                .sorted(Map.Entry.comparingByKey())
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        Map.Entry::getValue,
-                        (e1, e2) -> e1,
-                        LinkedHashMap::new
-                ));
+        Human human = new Human.Builder()
+                .setName("Alex")
+                .setAge(30)
+                .setGender("Man")
+                .setIsEmployed(true)
+                .build();
+        System.out.println(human.toString());
     }
 }
