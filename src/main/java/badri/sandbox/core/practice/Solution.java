6a227231ddc13d5d521611a877a0246ec724f63e
Marvelbad <badra.aqua@gmail.com>
2025-10-31 22:15:28 +0300

    Practice

--- src/main/java/badri/sandbox/core/practice/Solution.java
+++ src/main/java/badri/sandbox/core/practice/Solution.java
@@ -1,15 +1,25 @@
 package badri.sandbox.core.practice;
 
-import java.awt.print.Book;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class Solution {
     public static void main(String[] args) {
-        Human human = new Human.Builder()
-                .setName("Alex")
-                .setAge(30)
-                .setGender("Man")
-                .setIsEmployed(true)
-                .build();
-        System.out.println(human.toString());
+        List<City> cities = new ArrayList<>();
+
+        cities.add(new City("Sukhum", 60000));
+        cities.add(new City("Gagra", 20000));
+
+        Collections.sort(cities);
+
+        for (City city : cities) {
+            System.out.println(city);
+        }
     }
 }
